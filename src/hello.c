7c446a7f27cd4cce105c52847e3eec64e1ae0af0
vamsipa <vamsipallapothula@gmail.com>
2016-11-24 23:13:24 -0600

    Creted helloworld .c working file

---
+++ src/hello.c
@@ -0,0 +1,20 @@
+/*
+ * hello.c
+ *
+ *  Created on: Nov 24, 2016
+ *      Author: vamsi
+ */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	puts("******Hello world ********");
+
+
+return 0;
+}
